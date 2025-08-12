f93fb69ba2fcf50476d2ce172fd3e4213e583dcf
Matsumoto Teruo <rainsea.teruo@gmail.com>
2025-08-12 21:45:28 +0900

    db: thêm link tới postgres để lưu dữ liệu vào

--- config.py
+++ config.py
@@ -13,7 +13,8 @@ class Config:
     SECRET_KEY = os.environ.get(
         'SECRET_KEY') or '131fd895c8b23169dcd7d6df3275704dcd1908b12dbbe6b4e0adbbc03375daed'
     # Đường dẫn tuyệt đối tới jobs.db
-    uri = os.environ.get('DATABASE_URL')
+    uri = os.environ.get(
+        'postgresql://job_tracker_alur_user:PeSTccU6vwtjHxngGxzX7yocU7BE4Byv@dpg-d2djbqqdbo4c73bit78g-a/job_tracker_alur')
     if uri and uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgressql://", 1)
     SQLALCHEMY_DATABASE_URI = uri or (
